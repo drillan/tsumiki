1788a0e749c886d8fe8014fd2bf7fb2d16eec9d4
driller <eleshis@gmail.com>
2018-09-16 15:51:10 +0900

    add jinja2schema to install_requires

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="tsumiki",
-    version="0.0.1a",
+    version="0.0.1c",
     url="https://github.com/drillan/tsumiki",
     author="driller",
     description="Jupyter(IPython) cell magic for display multiple columns [Markdown/HTML].",
     python_requires=">=3.6",
-    install_requires=["markdown", "py-gfm", "jinja2"],
+    install_requires=["markdown", "py-gfm", "jinja2", "jinja2schema"],
     license="MIT",
     # py_modules=["tsumiki"],
     packages=find_packages(),
